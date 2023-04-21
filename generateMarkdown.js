eee7b3af4c742a27ab66743c829a81fad757dc4b
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-04-20 20:08:34 -0700

    markdown fixes
    added replace method to remove spaces and replace with dashes in the project name when creating the link

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,23 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-// make if statement to check what license we picked and return approipriate badge
+  // make if statement to check what license we picked and return approipriate badge
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.projectname}
+  // const projectLink creates a working link that replaces spaces in the project name with dashes
+  const projectLink = `https://github.com/${data.userName}/${data.projectName.replace(/\s+/g, '-')}`;
+  return `# ${data.projectName}
 ${renderLicenseBadge(data.license)}
 ## Description
 ${data.description}
+## Installation instructions
+${data.installationInstructions}
+## Usage information
+${data.usageInformation}
+## Contributing guidelines
+${data.contributingGuidelines}
+## Test instructions
+${data.testInstructions}
+## Links
+- [Repository](${projectLink})
+- [Website](https://seantamturk.github.io/Work-Day-Scheduler/)
+- [Linkedin](https://www.linkedin.com/in/sean-tamturk-8253b722a/)
+- [Site Image](./images/websiteimage.png)
 `;
 }
 
--- userReadme.md
+++ userReadme.md
@@ -1,4 +1,17 @@
-# undefined
+# test name
 undefined
 ## Description
-test description
+gsdfgfsdgdfsdfg
+## Installation instructions
+dfgdfgfgdfg
+## Usage information
+dfggdfdfggdf
+## Contributing guidelines
+fgdgfdgdfgdfdfg
+## Test instructions
+dfggdfdfggdf
+## Links
+- [Repository](https://github.com/seantamturk/test-name)
+- [Website](https://seantamturk.github.io/Work-Day-Scheduler/)
+- [Linkedin](https://www.linkedin.com/in/sean-tamturk-8253b722a/)
+- [Site Image](./images/websiteimage.png)
